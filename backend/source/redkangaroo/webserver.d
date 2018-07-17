9e5cdd60bbdef133dba315120b3226a826468251
Dmitry Sidorov <jonmagon@gmail.com>
2018-07-17 23:25:10 +0300

    Error handler was added

--- backend/source/redkangaroo/webserver.d
+++ backend/source/redkangaroo/webserver.d
@@ -10,7 +10,13 @@ import api.user;
 
 class WebServer {
 private:
-	void checkRequest(HTTPServerRequest req, HTTPServerResponse) {
+	static void checkRequest(HTTPServerRequest req, HTTPServerResponse) {
+		if (Config.RedKangaroo.allowGetInfo) {
+			logFatal("The request was rejected. " ~
+				"It needs to set the value of allowGetInfo to false.");
+			throw new HTTPStatusException(HTTPStatus.internalServerError);
+		}
+		
 		if (!new OTP().checkToken(req.headers.get("Token"))) {
 			logError("[IP: %s]: Invalid token.",
 				req.clientAddress.toAddressString(), req.requestURI);
@@ -18,7 +24,28 @@ private:
 		}
 	}
 	
-	void unspecifiedRoute(HTTPServerRequest req, HTTPServerResponse) {
+	static void getInfo(HTTPServerRequest, HTTPServerResponse res) {
+		import std.datetime;
+		import std.conv;
+		
+		if (!Config.RedKangaroo.allowGetInfo)
+			throw new HTTPStatusException(HTTPStatus.forbidden);
+		
+		res.writePrettyJsonBody(
+			Json([
+				"token": Json(Config.RedKangaroo.token),
+				"stepWindow": Json(Config.RedKangaroo.stepWindow),
+				"timestamp": Json(Clock.currTime(UTC()).toUnixTime())
+			])
+		);
+	}
+	
+	static void errorHandler(HTTPServerRequest req, HTTPServerResponse res,
+							 HTTPServerErrorInfo error) {
+		res.writePrettyJsonBody(Json(["error": Json(error.code)]));
+	}
+	
+	static void unspecifiedRoute(HTTPServerRequest req, HTTPServerResponse) {
 		logWarn("[IP: %s]: A route with name '%s' could not be found. " ~
 			"Check that `name: '%2$s'` was specified in the route's config.",
 			req.clientAddress.toAddressString(), req.requestURI);
@@ -31,22 +58,26 @@ public:
 		 * HTTPServerSettings.errorPageHandler doesn't working
 		 */
 		
-		auto route = new URLRouter;
+		auto router = new URLRouter;
 		
-		route.any("*", &checkRequest);
+		router.any("/info", &getInfo);
+		
+		router.any("*", &checkRequest);
 		
 		// Interfaces
-		registerRestInterface(route, new APISystem());
-		registerRestInterface(route, new APIUser());
+		registerRestInterface(router, new APISystem());
+		registerRestInterface(router, new APIUser());
 
-		route.any("*", &unspecifiedRoute);
+		router.any("*", &unspecifiedRoute);
 		
 		auto settings = new HTTPServerSettings;
 
+		settings.errorPageHandler = toDelegate(&errorHandler);
+		
 		settings.port = Config.RedKangaroo.port;
 		settings.bindAddresses = [Config.RedKangaroo.host];
 
-		listenHTTP(settings, route);
+		listenHTTP(settings, router);
 
 		runApplication();
 	}
